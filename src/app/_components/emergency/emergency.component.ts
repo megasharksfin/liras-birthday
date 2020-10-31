b989d2e5cbc6ae427c29e0c110bf5c4d809bd17d
megasharksfin <53161366+megasharksfin@users.noreply.github.com>
2020-10-31 20:53:17 +0800

    FINAL DEPLOYMENT

---
+++ src/app/_components/emergency/emergency.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-emergency',
+  templateUrl: './emergency.component.html',
+  styleUrls: ['./emergency.component.scss'],
+})
+export class EmergencyComponent implements OnInit {
+  isClicked = false;
+  constructor(
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  emergencyClick(): void {
+    this.isClicked = true;
+
+    const audio = new Audio();
+    audio.src = '../../assets/emergency.wav';
+    audio.load();
+    audio.play();
+
+    setTimeout(() => {
+      this.isClicked = false;
+      this.router.navigate([ '/message' ]);
+    }, 3000);
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './_pages/home/home.component';
+import { MessageContentComponent } from './_pages/message-content/message-content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'happy-birthday-lira', component: HomeComponent },
+      { path: '', redirectTo: 'happy-birthday-lira', pathMatch: 'full' },
+    ],
+  },
+  {
+    path: '',
+    children: [
+      { path: 'message', component: MessageContentComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
